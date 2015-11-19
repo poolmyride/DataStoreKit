fdebe9876470cc849e9f3f722e30ebb6a9717d7c
Rajat Talwar <cuterajat26@gmail.com>
2015-11-19 12:50:04 +0530

    added json support in FileModel

---
+++ Example/Tests/FileModelTests.swift
@@ -0,0 +1,80 @@
+//
+//  FileModelTests.swift
+//  DataStoreKit
+//
+//  Created by Rohit Talwar on 19/11/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import DataStoreKit
+class FileModelTests: XCTestCase {
+
+    class Animal:ObjectCoder{
+        var name:String?
+        var type:String?
+        required init(dictionary withDictionary: NSDictionary) {
+            self.name = withDictionary["name"] as? String
+            self.type = withDictionary["type"] as? String
+        }
+        func toDictionary() -> NSDictionary {
+            return [:]
+        }
+        
+        static func identifierKey() -> String {
+            return "name"
+        }
+    }
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testJsonQuery() {
+        
+        let model = FileModel<Animal>(path: "animals.json")
+        let expectation = self.expectationWithDescription("testJsonQuery")
+        model.query { (err, results) -> Void in
+            
+            let animals  = results as? [Animal]
+            
+            XCTAssert(animals?.count == 2,"Pass")
+            XCTAssert(animals?[0].name == "dog")
+            XCTAssertNotNil(animals)
+            expectation.fulfill()
+            
+        }
+        
+        self.waitForExpectationsWithTimeout(5, handler: nil)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPlistQuery() {
+        
+        let model = FileModel<Animal>(path: "animals.plist")
+        let expectation = self.expectationWithDescription("testPlistQuery")
+        model.query { (err, results) -> Void in
+            
+            let animals  = results as? [Animal]
+            
+            XCTAssert(animals?.count == 2,"Pass")
+            XCTAssert(animals?[0].name == "dog")
+            XCTAssertNotNil(animals)
+            expectation.fulfill()
+            
+        }
+        
+        self.waitForExpectationsWithTimeout(5, handler: nil)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+
+}
--- Pod/Classes/FileDeserializer.swift
+++ Pod/Classes/FileDeserializer.swift
@@ -13,17 +13,40 @@ import Foundation
 public class FileDeserializer<T where T:AnyObject,T:ObjectCoder> {
     
     
+    private func isPList(fileName:String) -> Bool{
+        return fileName.hasSuffix("plist")
+    }
+    
+    private func getJsonArray(fileName:String) -> NSArray? {
+        
+        let url = NSBundle.mainBundle().URLForResource(fileName, withExtension: "");
+
+        var jsonarray:NSArray? = nil
+        do {
+            try jsonarray = NSJSONSerialization.JSONObjectWithData(NSData(contentsOfURL: url!)!, options: []) as? NSArray
+        } catch _ as NSError {
+            
+        }
+        return jsonarray
+    }
+    
+    private func getPlistArray(fileName:String) -> NSArray? {
+        
+        let url = NSBundle.mainBundle().URLForResource(fileName, withExtension: "");
+        
+        let array:NSArray? = (url != nil) ? NSArray(contentsOfURL: url!)  : nil
+
+        return array
+    }
+    
     public  func getObjectArrayFrom(fielName fileName:String,callback: (NSError?,NSArray?)->Void) {
 
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), { () -> Void in
             
-            let url = NSBundle.mainBundle().URLForResource(fileName, withExtension: "");
-            
-            let array:NSArray? = (url != nil) ? NSArray(contentsOfURL: url!)  : []
-            
+            let array = (self.isPList(fileName) ? self.getPlistArray(fileName) : self.getJsonArray(fileName)) ?? []
             let objectDeserializer = ObjectDeserializer<T>();
-            let results:NSArray =  objectDeserializer.deSerializeArray(array!)
+            let results:NSArray =  objectDeserializer.deSerializeArray(array)
             
             dispatch_async(dispatch_get_main_queue(), { () -> Void in
                 callback(nil,results)
