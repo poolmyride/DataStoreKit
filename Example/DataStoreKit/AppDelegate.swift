069c80659a97330285de95f7cb198efcd5686f37
Rahul Taneja <rahul.taneja13@gmail.com>
2016-09-29 11:37:07 +0530

    appdelegate function changed

--- Example/DataStoreKit/AppDelegate.swift
+++ Example/DataStoreKit/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
